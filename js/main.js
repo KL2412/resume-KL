13fd20ef2445cb127ad3ed5f2cec2caff0ed8dd2
KL2412 <nicksona36@gmail.com>
2023-08-12 00:36:46 +0800

    Add gallery section

--- js/main.js
+++ js/main.js
@@ -115,3 +115,11 @@
 	});
 
 })(jQuery);
+
+function replaceContentInContainer(target, source) {
+	document.getElementById(target).innerHTML = document.getElementById(source).innerHTML;
+  }
+  
+  function replaceContentInOtherContainer(replace_target, source) {
+	document.getElementById(replace_target).innerHTML = document.getElementById(source).innerHTML;
+  }
